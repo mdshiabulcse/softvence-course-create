93ca9d12181efce5e0573c9d623d746021f0188f
shiabulislam <md.shiabul.cse@gmail.com>
2025-10-27 12:36:51 +0600

    add route cach optimization

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,39 @@ use App\Http\Controllers\CourseController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Artisan;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/route-cache', function() {
+    Artisan::call('route:clear');
+    return 'Routes cache has been cleared';
+});
+Route::get('/migrate', function() {
+    Artisan::call('migrate');
+    return 'Migrated Done';
+});
+Route::get('/storage-link', function() {
+    Artisan::call('storage:link');
+    return 'Storage Link Done';
+});
+Route::get('/db-seed', function() {
+    Artisan::call('db:seed');
+    return 'DB Seed Done';
+});
 
 Route::get('/', [HomeController::class, 'welcome'])->name('welcome');
 Route::get('/courses', [CourseController::class, 'publicIndex'])->name('courses.public');
